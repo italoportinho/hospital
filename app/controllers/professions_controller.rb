a30abf4c0a06fb061642536b831a49fcc9181ca7
unknown <Italo@portinho.huap.uff.br.local>
2012-04-26 21:06:52 -0300

    views de Profession criadas. Controller criado, falta terminar.

--- app/controllers/professions_controller.rb
+++ app/controllers/professions_controller.rb
@@ -1,2 +1,52 @@
 class ProfessionsController < ApplicationController
+
+  def index
+    @professions = Profession.all
+    respond_to do |format|
+      format.html
+      format.json {render json: @professions}
+    end
+  end
+
+  def show
+    @profession = Profession.find(params[:id])
+    respond_to do  |format|
+      format.html
+      format.json {render json: @profession}
+    end
+  end
+
+  def new
+    @profession = Profession.new
+    respond_to do  |format|
+      format.html
+      format.json {render json: @profession}
+    end
+  end
+
+  def edit
+
+  end
+
+  def create
+    @profession = Profession.new(params[:profession])
+    respond_to do |format|
+      if @profession.save
+        format.html {redirect_to @profession, notice: "Profession was successfully created."}
+        format.json {render json: @profession, status: :created, location: @profession}
+      else
+        format.html { render action: "new" }
+        format.json { render json: @profession.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
 end
